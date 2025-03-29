e8e98cc8613cfcbf6705cf2a12e540739a7beeeb
Peter Vaiko <peter.vaiko@siemens.com>
2025-03-29 07:44:26 -0400

    feat: graph pathfinder
    Add all points of the edges to the path.
    
    Add unit tests.

--- scripts/pathfinder/GraphPathfinder.lua
+++ scripts/pathfinder/GraphPathfinder.lua
@@ -53,6 +53,39 @@ function GraphPathfinder.GraphEdge:getExit(entry)
     end
 end
 
+function GraphPathfinder.GraphEdge:rollUpIterator(entry)
+    local from, to, step
+    if entry == self[1] then
+        -- unidirectional, or bidirectional, travelling from the start to end, roll up backwards
+        from, to, step = #self + 1, 1, -1
+    else
+        from, to, step = 0, #self, 1
+    end
+    local i = from
+    return function()
+        i = i + step
+        if i == to + step then
+            return nil, nil
+        else
+            return i, self[i]
+        end
+    end
+end
+
+--- A pathfinder node, specialized for the GraphPathfinder
+---@class GraphPathfinder.Node : State3D
+GraphPathfinder.Node = CpObject(State3D)
+
+---@param edge GraphPathfinder.GraphEdge the edge leading to this node: when rolling up the path, we need to add all
+--- vertices of the edge
+---@param entry Vector the entry point to this edge (when bidirectional, we may be travelling the edge from the end to
+--- the start.
+function GraphPathfinder.Node:init(x, y, g, pred, d, edge, entry)
+    State3D.init(self, x, y, 0, g, pred, Gear.Forward, Steer.Straight, 0, d)
+    self.edge = edge
+    self.entry = entry
+end
+
 ---@param yieldAfter number coroutine yield after so many iterations (number of iterations in one update loop)
 ---@param maxIterations number maximum iterations before failing
 ---@param range number when an edge's exit is closer than range to another edge's entry, the
@@ -73,6 +106,28 @@ end
 
 function GraphPathfinder:getMotionPrimitives(turnRadius, allowReverse)
     return GraphPathfinder.GraphMotionPrimitives(self.range, self.graph)
+end
+
+--- Override path roll up since here, the path also includes all edges of the graph, not just the pathfinder nodes
+---@param lastNode GraphPathfinder.Node
+function GraphPathfinder:rollUpPath(lastNode, goal, path)
+    path = path or {}
+    local currentNode = lastNode
+    self:debug('Goal node at %.2f/%.2f, cost %.1f (%.1f - %.1f)', goal.x, goal.y, lastNode.cost,
+            self.nodes.lowestCost, self.nodes.highestCost)
+    while currentNode.pred and currentNode ~= currentNode.pred do
+        if currentNode.edge then
+            -- add the edge leading to the node
+            for _, node in currentNode.edge:rollUpIterator(currentNode.entry) do
+                table.insert(path, 1, node)
+            end
+        end
+        currentNode = currentNode.pred
+    end
+    table.insert(path, 1, currentNode)
+    self:debug('Nodes %d, iterations %d, yields %d, deltaTheta %.1f', #path, self.iterations, self.yields,
+            math.deg(self.deltaThetaGoal))
+    return path
 end
 
 --- Motion primitives to use with the graph pathfinder, providing the entries
@@ -98,7 +153,8 @@ function GraphPathfinder.GraphMotionPrimitives:getPrimitives(node)
             local distanceToEntry = (node - entry):length()
             if distanceToEntry <= self.range then
                 local exit = edge:getExit(entry)
-                table.insert(primitives, {x = exit.x, y = exit.y, d = edge:getLength() + distanceToEntry} )
+                table.insert(primitives, { x = exit.x, y = exit.y, d = edge:getLength() + distanceToEntry,
+                                           edge = edge, entry = entry })
             end
         end
     end
@@ -107,7 +163,6 @@ end
 
 ---@return State3D successor for the given primitive
 function GraphPathfinder.GraphMotionPrimitives:createSuccessor(node, primitive, hitchLength)
-    return State3D(primitive.x, primitive.y, 0, node.g, node, node.gear, node.steer,
-            0, node.d + primitive.d)
+    return GraphPathfinder.Node(primitive.x, primitive.y, node.g, node, node.d + primitive.d, primitive.edge, primitive.entry)
 end
 
---
+++ scripts/pathfinder/test/GraphPathfinderTest.lua
@@ -0,0 +1,181 @@
+package.path = package.path .. ";../../test/?.lua;../../geometry/?.lua;../../courseGenerator/geometry/?.lua;../../courseGenerator/?.lua;../?.lua;../../?.lua;../../util/?.lua"
+lu = require("luaunit")
+require('mock-GiantsEngine')
+require('mock-Courseplay')
+require('CpObject')
+require('CpUtil')
+require('Logger')
+require('BinaryHeap')
+require('CpMathUtil')
+require('Dubins')
+require('ReedsShepp')
+require('ReedsSheppSolver')
+require('AnalyticSolution')
+require('CourseGenerator')
+require('WaypointAttributes')
+require('Vector')
+require('LineSegment')
+require('State3D')
+require('Vertex')
+require('Polyline')
+require('Polygon')
+require('PathfinderUtil')
+require('HybridAStar')
+require('GraphPathfinder')
+
+local GraphEdge = GraphPathfinder.GraphEdge
+local TestConstraints = CpObject(PathfinderConstraintInterface)
+local pathfinder, start, goal, done, path, goalNodeInvalid
+local function printPath()
+    for _, p in ipairs(path) do
+        print(p)
+    end
+end
+
+function testDirection()
+    local graph = {
+        GraphEdge(GraphEdge.UNIDIRECTIONAL,
+                {
+                    Vertex(100, 100),
+                    Vertex(110, 100),
+                    Vertex(120, 100)
+                }),
+        GraphEdge(
+                GraphEdge.UNIDIRECTIONAL,
+                {
+                    Vertex(120, 105),
+                    Vertex(110, 105),
+                    Vertex(100, 105),
+                }),
+    }
+    pathfinder = GraphPathfinder(math.huge, 500, 20, graph)
+    start = State3D(90, 105, 0, 0)
+    goal = State3D(130, 105, 0, 0)
+    done, path, goalNodeInvalid = pathfinder:run(start, goal, 1, false, TestConstraints(), 0)
+    lu.assertIsTrue(done)
+    lu.assertEquals(#path, 4)
+    -- path contains the start node and all points of the edge it goes through
+    lu.assertEquals(start, path[1])
+    path[2]:assertAlmostEquals(Vector(100, 100))
+    path[3]:assertAlmostEquals(Vector(110, 100))
+    path[#path]:assertAlmostEquals(Vector(120, 100))
+    start, goal = goal, start
+    done, path, goalNodeInvalid = pathfinder:run(start, goal, 1, false, TestConstraints(), 0)
+    lu.assertIsTrue(done)
+    lu.assertEquals(#path, 4)
+    lu.assertEquals(start, path[1])
+    path[2]:assertAlmostEquals(Vector(120, 105))
+    path[3]:assertAlmostEquals(Vector(110, 105))
+    path[#path]:assertAlmostEquals(Vector(100, 105))
+end
+
+function testBidirectional()
+    local graph = {
+        GraphEdge(GraphEdge.BIDIRECTIONAL,
+                {
+                    Vertex(120, 100),
+                    Vertex(110, 100),
+                    Vertex(100, 100),
+                }),
+        GraphEdge(
+                GraphEdge.UNIDIRECTIONAL,
+                {
+                    Vertex(120, 105),
+                    Vertex(110, 105),
+                    Vertex(100, 105),
+                }),
+    }
+    pathfinder = GraphPathfinder(math.huge, 500, 20, graph)
+    start = State3D(90, 105, 0, 0)
+    goal = State3D(130, 105, 0, 0)
+    done, path, goalNodeInvalid = pathfinder:run(start, goal, 1, false, TestConstraints(), 0)
+    lu.assertIsTrue(done)
+    lu.assertEquals(#path, 4)
+    -- path contains the start node and all points of the edge it goes through
+    lu.assertEquals(start, path[1])
+    path[2]:assertAlmostEquals(Vector(100, 100))
+    path[3]:assertAlmostEquals(Vector(110, 100))
+    path[#path]:assertAlmostEquals(Vector(120, 100))
+    start, goal = goal, start
+    done, path, goalNodeInvalid = pathfinder:run(start, goal, 1, false, TestConstraints(), 0)
+    lu.assertIsTrue(done)
+    lu.assertEquals(#path, 4)
+    lu.assertEquals(start, path[1])
+    -- TODO: here, it should have taken the other path, over y = 105, as it is slightly shorter since both start and
+    -- goal are on y = 105, but since we reach the goal in a single step,
+    -- it just goes with the first one it finds. This isn't the hill we want to die on, so for now,
+    -- we will just accept this behavior.
+    path[2]:assertAlmostEquals(Vector(120, 100))
+    path[3]:assertAlmostEquals(Vector(110, 100))
+    path[#path]:assertAlmostEquals(Vector(100, 100))
+end
+
+function testShorterPath()
+    local graph = {
+        GraphEdge(GraphEdge.UNIDIRECTIONAL,
+                {
+                    Vertex(100, 100),
+                    Vertex(110, 100),
+                    Vertex(120, 100)
+                }),
+        GraphEdge(
+                GraphEdge.UNIDIRECTIONAL,
+                {
+                    Vertex(100, 105),
+                    Vertex(110, 200),
+                    Vertex(120, 105),
+                }),
+    }
+    pathfinder = GraphPathfinder(math.huge, 500, 20, graph)
+    start = State3D(90, 105, 0, 0)
+    goal = State3D(130, 105, 0, 0)
+    done, path, goalNodeInvalid = pathfinder:run(start, goal, 1, false, TestConstraints(), 0)
+    lu.assertIsTrue(done)
+    lu.assertEquals(#path, 4)
+    -- path contains the start node and all points of the edge it goes through
+    lu.assertEquals(start, path[1])
+    path[2]:assertAlmostEquals(Vector(100, 100))
+    path[3]:assertAlmostEquals(Vector(110, 100))
+    path[#path]:assertAlmostEquals(Vector(120, 100))
+end
+
+function testRange()
+    local graph = {
+        GraphEdge(GraphEdge.UNIDIRECTIONAL,
+                {
+                    Vertex(100, 100),
+                    Vertex(110, 100),
+                    Vertex(120, 100)
+                }),
+        GraphEdge(
+                GraphEdge.UNIDIRECTIONAL,
+                {
+                    Vertex(120, 105),
+                    Vertex(110, 105),
+                    Vertex(100, 105),
+                }),
+        GraphEdge(GraphEdge.UNIDIRECTIONAL,
+                {
+                    Vertex(130, 100),
+                    Vertex(140, 100),
+                    Vertex(150, 100)
+                }),
+    }
+    pathfinder = GraphPathfinder(math.huge, 500, 20, graph)
+    start = State3D(90, 105, 0, 0)
+    goal = State3D(150, 105, 0, 0)
+    done, path, goalNodeInvalid = pathfinder:run(start, goal, 1, false, TestConstraints(), 0)
+    lu.assertIsTrue(done)
+    lu.assertEquals(#path, 7)
+    -- path contains the start node and all points of the edge it goes through
+    lu.assertEquals(start, path[1])
+    path[2]:assertAlmostEquals(Vector(100, 100))
+    path[3]:assertAlmostEquals(Vector(110, 100))
+    path[#path]:assertAlmostEquals(Vector(150, 100))
+    pathfinder = GraphPathfinder(math.huge, 500, 10, graph)
+    done, path, goalNodeInvalid = pathfinder:run(start, goal, 1, false, TestConstraints(), 0)
+    lu.assertIsNil(path)
+end
+
+
+os.exit(lu.LuaUnit.run())
