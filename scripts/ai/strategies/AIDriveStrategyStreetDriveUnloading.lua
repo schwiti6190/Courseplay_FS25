e4ceb87b6f570522a5d5a7ecc426811f39d2a926
David Schwietering <d-schwietering@gmx.de>
2025-04-11 23:21:59 +0200

    WIP baleloader unload

--- scripts/ai/strategies/AIDriveStrategyStreetDriveUnloading.lua
+++ scripts/ai/strategies/AIDriveStrategyStreetDriveUnloading.lua
@@ -14,7 +14,20 @@ end
 
 function AIDriveStrategyStreetDriveUnloading:initializeImplementControllers(vehicle)
     AIDriveStrategyStreetDriveToPoint.initializeImplementControllers(self, vehicle)
+    self.baleLoader = self:addImplementController(vehicle, BaleLoaderController, BaleLoader)
+    self.hasAutoLoader = self:addImplementController(vehicle, UniversalAutoloadController, nil, {}, "spec_universalAutoload") ~= nil
+end
 
+function AIDriveStrategyStreetDriveUnloading:setAllStaticParameters()
+    AIDriveStrategyStreetDriveToPoint.setAllStaticParameters(self)
+   
+end
+
+function AIDriveStrategyStreetDriveUnloading:delete()
+    AIDriveStrategyStreetDriveToPoint.delete(self)
+    if self.lastWaypointNode then 
+        self.lastWaypointNode:destroy()
+    end
 end
 
 function AIDriveStrategyStreetDriveUnloading:drivingCourse()
@@ -34,24 +47,48 @@ function AIDriveStrategyStreetDriveUnloading:onCourseEndReached()
     self:setCurrentTaskFinished()
 end
 
+---@param course Course
+---@param ix number
+function AIDriveStrategyStreetDriveUnloading:startDrivingCourse(course, ix)
+    AIDriveStrategyStreetDriveToPoint.startDrivingCourse(self, course, ix)
+    -- ---@type WaypointNode
+    -- self.lastWaypointNode = WaypointNode("lastWaypoint")
+    -- self.lastWaypointNode:setToWaypoint(course, course:getNumberOfWaypoints())
+end
+
 function AIDriveStrategyStreetDriveUnloading:updateUnloading()
     local targetDischargeNode, targetObject
     local isDischarging = false
-    local nodes, nodesToObject = AIUtil.getAllDischargeNodes(self.vehicle)
-    for _, node in pairs(nodes) do 
-        isDischarging = isDischarging or nodesToObject[node]:getDischargeState() == Dischargeable.DISCHARGE_STATE_OBJECT
-        if node.dischargeObject ~= nil then
-            targetDischargeNode = node
-            targetObject = nodesToObject[node]
+
+    if self.baleLoader ~= nil then 
+        local course = self.ppc:getCourse()
+        if course:isCloseToLastWaypoint(1) then 
+            if self.baleLoader:getIsAutomaticBaleUnloadingAllowed() then
+                self.baleLoader:startAutomaticBaleUnloading()
+                self:setMaxSpeed(0)
+            elseif self.baleLoader:getIsAutomaticBaleUnloadingInProgress() then
+                self:setMaxSpeed(0)
+            end
         end
-    end
-    if isDischarging then 
-        self:setMaxSpeed(0)
-    elseif targetObject then
-        self:setMaxSpeed(0)
-        if targetObject:getCanDischargeToObject(targetDischargeNode) then
-            targetObject:setCurrentDischargeNodeIndex(targetDischargeNode.index)
-            targetObject:setDischargeState(Dischargeable.DISCHARGE_STATE_OBJECT)
+    elseif self.hasAutoLoader then
+        --- TODO
+    else
+        local nodes, nodesToObject = AIUtil.getAllDischargeNodes(self.vehicle)
+        for _, node in pairs(nodes) do 
+            isDischarging = isDischarging or nodesToObject[node]:getDischargeState() == Dischargeable.DISCHARGE_STATE_OBJECT
+            if node.dischargeObject ~= nil then
+                targetDischargeNode = node
+                targetObject = nodesToObject[node]
+            end
+        end
+        if isDischarging then 
+            self:setMaxSpeed(0)
+        elseif targetObject then
+            self:setMaxSpeed(0)
+            if targetObject:getCanDischargeToObject(targetDischargeNode) then
+                targetObject:setCurrentDischargeNodeIndex(targetDischargeNode.index)
+                targetObject:setDischargeState(Dischargeable.DISCHARGE_STATE_OBJECT)
+            end
         end
     end
 end
